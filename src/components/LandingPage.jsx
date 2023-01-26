f624de27bf644f0802590dcbeede3d7e65da9a67
Your Name <julian.sonido@gmail.com>
2023-01-26 12:45:40 -0300

    landing page changes

--- src/components/LandingPage.jsx
+++ src/components/LandingPage.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
-import { NavLink } from 'react-router-dom';
+import {useHistory } from 'react-router-dom';
 import styles from "./CSS/LandingPage.module.css";
 
 
 export default function LandingPage() {
+
+  let history = useHistory();
+
+  let handleClick = () => {
+    history.push("/home")
+  }
+
   return (
     <div className={styles.backgroundPicture}>
 
-        <NavLink className={styles.navLink} exact to="/home" >
+        <button 
+        className={styles.newButton}
+        onClick={handleClick}
+        >
+          <p>BRING THE DOGGIES!</p>
+        </button> 
 
-          <button className={styles.button}>
-            <p>BRING..</p>
-            <p>THE..</p>
-            <p>DOGGIES!</p>
-          </button>
-
-        </NavLink>
-        
     </div>
   )
 }
